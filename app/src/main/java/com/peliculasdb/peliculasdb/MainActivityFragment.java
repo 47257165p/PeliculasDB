88f046254e0ee578bd2b2ed2d308867bdd970b50
Alejandro <47257165p@giespoblenou.org>
2015-10-26 18:01:21 +0100

    Creado el ListView no funcional, primeros pasos del programa y direccionando views. -Alejandro Soriano

--- app/src/main/java/com/peliculasdb/peliculasdb/MainActivityFragment.java
+++ app/src/main/java/com/peliculasdb/peliculasdb/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,10 +19,18 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
+    private ArrayList items;
+    private ArrayAdapter <String> adapter;
+    private ListView lv1;
+    private TextView tv1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        lv1 = (ListView) rootView.findViewById(R.id.lv1);
+        tv1 = (TextView) rootView.findViewById(R.id.tv1);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
