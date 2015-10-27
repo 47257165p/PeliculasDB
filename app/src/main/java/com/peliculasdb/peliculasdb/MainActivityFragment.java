3f1e16151cbc3baff48dee9d15727816a3aaf3fd
Alejandro <47257165p@giespoblenou.org>
2015-10-27 19:45:41 +0100

    Acabado ListView funcional y código comentado. -Alejandro Soriano

--- app/src/main/java/com/peliculasdb/peliculasdb/MainActivityFragment.java
+++ app/src/main/java/com/peliculasdb/peliculasdb/MainActivityFragment.java
@@ -9,7 +9,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,15 +24,36 @@ public class MainActivityFragment extends Fragment {
     private ArrayList items;
     private ArrayAdapter <String> adapter;
     private ListView lv1;
-    private TextView tv1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        lv1 = (ListView) rootView.findViewById(R.id.lv1);
-        tv1 = (TextView) rootView.findViewById(R.id.tv1);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        //Creación de la vista del fragment.
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //Sincronizando la listView mediante el fragment.
+        lv1 = (ListView) rootView.findViewById(R.id.lv1);
+
+        String [] defecto = {"Película por defecto 1", "Película por defecto 2", "Película por defecto 3", "Película por defecto 4", "Película por defecto 5", "Película por defecto 6"};
+
+        //Insserción del String anterior dentro del ArrayList de items
+        items = new ArrayList(Arrays.asList(defecto));
+
+        //Creación del adapter mediante el ArrayList de items
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
+
+        //Al listView se le pasa el adapter cuyo contenido es el arrayList de items.
+        lv1.setAdapter(adapter);
+
+        //Finalmente se hace un return del rootview para la actualización de la actividad.
+        return rootView;
+
     }
 }
